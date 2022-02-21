5f0581671d99fb9b9b6699ac82a52db90d691129
SylwiaLeung <longdepati@gmail.com>
2022-02-21 13:21:13 +0100

    Implemented CrimeContext class

---
+++ RepCrime/CrimeAPI/Data/CrimeContext.cs
@@ -0,0 +1,19 @@
+﻿using CrimeService.Models;
+using MongoDB.Driver;
+
+namespace CrimeService.Data
+{
+    public class CrimeContext : ICrimeContext
+    {
+        public CrimeContext(IConfiguration configuration, ILogger<HealthbookContext> logger)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Healthbooks = database.GetCollection<Healthbook>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            HealthbookContextSeed.SeedData(Healthbooks, logger);
+        }
+
+        public IMongoCollection<Crime> Crimes { get; }
+    }
+}
