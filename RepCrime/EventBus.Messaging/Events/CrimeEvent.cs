8ecaa8bf038d2a8209e17bca0f2822e66c7471b9
SylwiaLeung <longdepati@gmail.com>
2022-02-21 22:33:01 +0100

    Updated crime event by adding new field

--- RepCrime/EventBus.Messaging/Events/CrimeEvent.cs
+++ RepCrime/EventBus.Messaging/Events/CrimeEvent.cs
@@ -9,6 +9,7 @@ namespace EventBus.Messaging.Events
         public string Description { get; set; }
         public DateTime DateOfCrime { get; set; }
         public string PlaceOfCrime { get; set; }
+        public string WitnessName { get; set; }
         public string WitnessEmail { get; set; }
         public Status Status { get; set; }
         public string EnforcementId { get; set; }
---
+++ RepCrime/EventBus.Messaging/Events/CrimeUpdateEvent.cs
@@ -0,0 +1,12 @@
+﻿using CommonItems.Enums;
+
+namespace EventBus.Messaging.Events
+{
+    public class CrimeUpdateEvent
+    {
+        public string Id { get; set; }
+        public string WitnessName { get; set; }
+        public string WitnessEmail { get; set; }
+        public Status Status { get; set; }
+    }
+}
