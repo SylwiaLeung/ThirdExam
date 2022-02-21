9e1297b11da5949b738932fe6cd990fd591e7627
SylwiaLeung <longdepati@gmail.com>
2022-02-21 16:16:48 +0100

    Implemented crime controller get methods

---
+++ RepCrime/CrimeAPI/Controllers/CrimesController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using CrimeService.Models.Dtos;
+using CrimeService.Services.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrimeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrimesController : ControllerBase
+    {
+        private readonly ICrimeRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CrimesController(ICrimeRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CrimeReadDto>>> GetCrimes()
+        {
+            var crimes = await _repository.GetCrimes();
+
+            return Ok(_mapper.Map<List<CrimeReadDto>>(crimes));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CrimeReadDto>> GetCrimeById(string id)
+        {
+            var crime = await _repository.GetCrimeById(id);
+
+            return Ok(_mapper.Map<CrimeReadDto>(crime));
+        }
+    }
+}
