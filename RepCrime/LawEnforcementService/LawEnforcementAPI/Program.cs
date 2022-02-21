8c3c462f8b917d13edd818b942d319ac1ca935c7
SylwiaLeung <longdepati@gmail.com>
2022-02-21 17:11:49 +0100

    added host configuration and reistered rabbitmq in program.cs

--- RepCrime/LawEnforcementService/LawEnforcementAPI/Program.cs
+++ RepCrime/LawEnforcementService/LawEnforcementAPI/Program.cs
@@ -1,13 +1,17 @@
+using EventBus.Messaging;
 using FluentValidation.AspNetCore;
 using LawEnforcement.Application;
 using LawEnforcement.Application.Behaviours;
 using LawEnforcement.Infrastructure;
 using LawEnforcement.Infrastructure.Persistence;
+using LawEnforcementAPI.EventBusConsumer;
+using MassTransit;
 using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // SERWISY
+
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices();
 
@@ -18,14 +22,34 @@ builder.Services.AddControllers().AddFluentValidation()
         s.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
     });
 
+//RABBITMQ
+
+builder.Services.AddMassTransit(config =>
+{
+
+    config.AddConsumer<CrimeEventConsumer>();
+
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+
+        cfg.ReceiveEndpoint(EventBusConstants.CrimeQueue, c =>
+        {
+            c.ConfigureConsumer<CrimeEventConsumer>(ctx);
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
 
 // SZWAGIER
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // PIPELINE
+
 EnforcementContextSeed.Initialize();
 
 if (app.Environment.IsDevelopment())
