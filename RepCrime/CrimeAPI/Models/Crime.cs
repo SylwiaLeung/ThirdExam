30d7f16758a8110137d873969c595d55b9f02abd
SylwiaLeung <longdepati@gmail.com>
2022-02-21 13:12:16 +0100

    Implemented Crime entity

---
+++ RepCrime/CrimeAPI/Models/Crime.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrimeService.Models
+{
+    public class Crime
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public CrimeType CrimeType { get; set; }
+        public string? Description { get; set; }
+        public string PlaceOfCrime { get; set; }
+        public string? WitnessEmail { get; set; }
+        public Status Status { get; set; } = Status.Waiting;
+        public string EnforcementId { get; set; }
+    }
+}
