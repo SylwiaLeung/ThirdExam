063a2120fd28d173999ae7d915a6dfd7a2fb0d4b
SylwiaLeung <longdepati@gmail.com>
2022-02-21 13:58:37 +0100

    Added configuration for mongo db connection

--- RepCrime/CrimeAPI/Data/CrimeContext.cs
+++ RepCrime/CrimeAPI/Data/CrimeContext.cs
@@ -5,13 +5,13 @@ namespace CrimeService.Data
 {
     public class CrimeContext : ICrimeContext
     {
-        public CrimeContext(IConfiguration configuration, ILogger<HealthbookContext> logger)
+        public CrimeContext(IConfiguration configuration, ILogger<CrimeContext> logger)
         {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
-            Healthbooks = database.GetCollection<Healthbook>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
-            HealthbookContextSeed.SeedData(Healthbooks, logger);
+            Crimes = database.GetCollection<Crime>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CrimeContextSeed.SeedData(Crimes, logger);
         }
 
         public IMongoCollection<Crime> Crimes { get; }
