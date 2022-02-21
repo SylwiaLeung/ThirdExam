1faee062da61385cbd6d7711cedf6bf96eda20f4
SylwiaLeung <longdepati@gmail.com>
2022-02-21 12:18:47 +0100

    Implemented getall method

---
+++ RepCrime/LawEnforcementService/LawEnforcementAPI/Controllers/EnforcementController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using LawEnforcement.Application.Contracts;
+using LawEnforcement.Domain.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LawEnforcementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnforcementController : ControllerBase
+    {
+        private readonly IEnforcementRepository _repository;
+        private readonly ILogger<EnforcementController> _logger;
+        private readonly IMapper _mapper;
+
+        public EnforcementController(IEnforcementRepository repository, ILogger<EnforcementController> logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EnforcementReadDto>>> GetAllEnforcement()
+        {
+            var enforcement = await _repository.GetAllAsync();
+
+            return Ok(_mapper.Map<List<EnforcementReadDto>>(enforcement));
+        }
+       
+    }
+}
