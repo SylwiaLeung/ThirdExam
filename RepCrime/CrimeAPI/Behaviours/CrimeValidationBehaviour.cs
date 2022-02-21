555843f70a5a2dbb18d5cc0ea133ad53a3879287
SylwiaLeung <longdepati@gmail.com>
2022-02-21 14:44:43 +0100

    Implemented crime mapping profile

--- RepCrime/CrimeAPI/Behaviours/CrimeValidationBehaviour.cs
+++ RepCrime/CrimeAPI/Behaviours/CrimeValidationBehaviour.cs
@@ -1,6 +1,16 @@
-﻿namespace CrimeService.Behaviours
+﻿using CrimeService.Models.Dtos;
+using FluentValidation;
+
+namespace CrimeService.Behaviours
 {
-    public class CrimeValidationBehaviour
+    public class CrimeValidationBehaviour : AbstractValidator<CrimeCreateDto>
     {
+        public CrimeValidationBehaviour()
+        {
+            RuleFor(x => x.Description).MaximumLength(100);
+            RuleFor(x => x.WitnessEmail).EmailAddress().NotEmpty();
+            RuleFor(x => x.CrimeType).IsInEnum().NotEmpty();
+            RuleFor(x => x.EnforcementId).NotEmpty().Length(6);
+        }
     }
 }
---
+++ RepCrime/CrimeAPI/MappingProfiles/CrimeMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CrimeService.Models;
+using CrimeService.Models.Dtos;
+
+namespace CrimeService.MappingProfiles
+{
+    public class CrimeMappingProfile : Profile
+    {
+        public CrimeMappingProfile()
+        {
+            CreateMap<CrimeCreateDto, Crime>();
+            CreateMap<Crime, CrimeReadDto>();
+        }
+    }
+}
