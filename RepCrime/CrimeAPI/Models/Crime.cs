28307638c86ffa99191824843c63d3e2708669a7
SylwiaLeung <longdepati@gmail.com>
2022-02-21 22:31:48 +0100

    Updated Crime entity and Dto by adding field WitnessName

--- RepCrime/CrimeAPI/Models/Crime.cs
+++ RepCrime/CrimeAPI/Models/Crime.cs
@@ -13,6 +13,7 @@ namespace CrimeService.Models
         public string? Description { get; set; }
         public DateTime DateOfCrime { get; set; } = DateTime.UtcNow;
         public string PlaceOfCrime { get; set; }
+        public string WitnessName { get; set; }
         public string? WitnessEmail { get; set; }
         public Status Status { get; set; } = Status.Waiting;
         public string EnforcementId { get; set; }
--- RepCrime/CrimeAPI/Models/Dtos/CrimeCreateDto.cs
+++ RepCrime/CrimeAPI/Models/Dtos/CrimeCreateDto.cs
@@ -7,6 +7,7 @@ namespace CrimeService.Models.Dtos
         public CrimeType CrimeType { get; set; }
         public string? Description { get; set; }
         public string PlaceOfCrime { get; set; }
+        public string WitnessName { get; set; }
         public string WitnessEmail { get; set; }
         public string EnforcementId { get; set; }
     }
