7880d1ef8da98df89795fd324af3ef26286106c8
SylwiaLeung <longdepati@gmail.com>
2022-02-21 14:11:07 +0100

    Implemented ICrimeRepository

---
+++ RepCrime/CrimeAPI/Services/Repositories/ICrimeRepository.cs
@@ -0,0 +1,11 @@
+﻿using CrimeService.Models;
+
+namespace CrimeService.Services.Repositories
+{
+    public interface ICrimeRepository
+    {
+        Task<IEnumerable<Crime>> GetCrime();
+        Task<Crime> GetCrimeById(string id);
+        Task AddCrime(Crime healthbook);
+    }
+}
