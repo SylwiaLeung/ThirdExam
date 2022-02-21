81c97d9ea4942aec8fa795db41b07a768efcde7e
SylwiaLeung <longdepati@gmail.com>
2022-02-21 16:34:41 +0100

    Implemented crimeEvent entity in a new library project EventBus.Messaging

---
+++ RepCrime/EventBus.Messaging/Events/CrimeEvent.cs
@@ -0,0 +1,21 @@
+﻿using CommonItems.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messaging.Events
+{
+    public class CrimeEvent
+    {
+        public string Id { get; set; }
+        public CrimeType CrimeType { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfCrime { get; set; }
+        public string PlaceOfCrime { get; set; }
+        public string WitnessEmail { get; set; }
+        public Status Status { get; set; }
+        public string EnforcementId { get; set; }
+    }
+}
