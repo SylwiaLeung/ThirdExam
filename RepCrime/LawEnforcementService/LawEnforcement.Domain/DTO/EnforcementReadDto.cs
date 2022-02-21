e97e669aa0713bff82a091417c740d088fb7c06e
SylwiaLeung <longdepati@gmail.com>
2022-02-21 12:20:46 +0100

    Commented unimplemented variables

--- RepCrime/LawEnforcementService/LawEnforcement.Domain/DTO/EnforcementReadDto.cs
+++ RepCrime/LawEnforcementService/LawEnforcement.Domain/DTO/EnforcementReadDto.cs
@@ -5,6 +5,6 @@
         public string Id { get; set; }
         public string Name { get; set; }
         public Rank Rank { get; set; }
-        public ICollection<CrimeEvent> Crimes { get; set; }
+        //public ICollection<CrimeEvent> Crimes { get; set; }
     }
 }
