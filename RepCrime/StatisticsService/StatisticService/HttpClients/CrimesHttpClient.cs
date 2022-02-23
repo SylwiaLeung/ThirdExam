d442af15e4357124efd132bf2dc0222c540874dc
SylwiaLeung <longdepati@gmail.com>
2022-02-23 21:14:09 +0100

    Created new project for statistics service

---
+++ RepCrime/StatisticsService/StatisticService/HttpClients/CrimesHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Formatting;
+
+namespace StatisticService.HttpClients
+{
+    public class CrimesHttpClient : ICrimesHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<CrimesHttpClient> _logger;
+
+        public CrimesHttpClient(HttpClient httpClient, IConfiguration configuration, ILogger<CrimesHttpClient> logger)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task<Dictionary<DateTime, int>> GetDailyCrimeRate()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{_configuration["CrimeService"]}/");
+            var response = await _httpClient.SendAsync(requestMessage);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+                return await response.Content.ReadAsAsync<Dictionary<DateTime, int>>(new[] { new JsonMediaTypeFormatter() });
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Task<Dictionary<string, int>> GetNumberOfCrimesPerOfficer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RepCrime/StatisticsService/StatisticService/HttpClients/ICrimesHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace StatisticService.HttpClients
+{
+    public interface ICrimesHttpClient
+    {
+        Task<Dictionary<DateTime, int>> GetDailyCrimeRate();
+        Task<Dictionary<string, int>> GetNumberOfCrimesPerOfficer();
+    }
+}
